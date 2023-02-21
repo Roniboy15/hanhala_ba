609bb98eb4f2e513b73bd54103c830f04c6327ae
Your Name <you@example.com>
2023-02-21 18:57:37 +0200

    ok

--- server/routes/configRoutes.js
+++ server/routes/configRoutes.js
@@ -1,4 +1,3 @@
-const indexR = require("./index");
 const usersR = require("./users");
 const applicantsR = require("./applicants");
 const datenR = require("./daten");
@@ -6,7 +5,6 @@ const housesR = require("./houses");
 
 
 exports.routesInit = (app) => {
-  app.use("/",indexR);
   app.use("/users",usersR);
   app.use("/applicants",applicantsR);
   app.use("/daten", datenR);
@@ -15,7 +13,7 @@ exports.routesInit = (app) => {
 
 // כל ראוט אחר שנגיע שלא קיים בתקיית פאליק או כראוט
 // נקבל 404
-  app.use("*",(req,res) => {
+ /* app.use("*",(req,res) => {
     res.status(404).json({msg:"endpoint not found , 404",error:404})
   })
-}
+}*/}
