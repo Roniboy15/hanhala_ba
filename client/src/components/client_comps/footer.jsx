1a66e1168a71839fd9dc499f5f53f9266cde2f9a
Your Name <you@example.com>
2023-03-01 19:06:26 +0200

    ok

--- client/src/components/client_comps/footer.jsx
+++ client/src/components/client_comps/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
             </div>
 
             <div className="text-center p-3 rounded-bottom-1" style={{ backgroundColor: "grey", opacity:"95%" }}>
-                © 2020 Copyright:
+                © 2023 Copyright:
                 <a className="text-white unstyled" target={'_blank'} href={`mailto:jaron.111@hotmail.com`}>Jaron Treyer</a>
             </div>
         </footer>
