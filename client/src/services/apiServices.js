123ecc474afd407bec4261fc82c3ffe5fbb6d091
Your Name <you@example.com>
2023-03-01 13:36:10 +0200

    ok

--- client/src/services/apiServices.js
+++ client/src/services/apiServices.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = "http://localhost:3006";
+export const API_URL = "";
 // export const API_URL = "http://blalba.cyclic.com";
 
 export const TOKEN_KEY = "ba_tok";
